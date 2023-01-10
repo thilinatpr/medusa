4d49b8fac13694368c32550b852e9ed8c0783f68
thilinatpr <thilinatpr@gmail.com>
2023-01-10 10:29:09 +0530

    hard cord db and redis url in config

--- medusa-config.js
+++ medusa-config.js
@@ -23,17 +23,17 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "https:/admin.thegift.lk";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL || "postgresql://postgres:OD4pVI5uaVydduovnRdN@containers-us-west-181.railway.app:7439/railway";
 
 // Medusa uses Redis, so this needs configuration as well
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || "redis://default:Ius3ljx52drCPW3Si20s@containers-us-west-184.railway.app:6818";
 
 // Stripe keys
 const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
