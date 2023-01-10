cfeb1925385579aa65820850610e9c8b47cd459d
thilinatpr <thilinatpr@gmail.com>
2023-01-10 10:33:30 +0530

    correct admin cors url

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "https:/admin.thegift.lk";
+const ADMIN_CORS = process.env.ADMIN_CORS || "https://admin.thegift.lk";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
