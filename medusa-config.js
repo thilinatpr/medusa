af0b6be7d3b4d5824b22288e26c19aea92a2977b
thilinatpr <thilinatpr@gmail.com>
2023-01-12 02:13:18 +0530

    change bucket url

--- medusa-config.js
+++ medusa-config.js
@@ -50,8 +50,7 @@ const plugins = [
         bucket: process.env.S3_BUCKET,
         access_key_id: process.env.S3_ACCESS_KEY_ID,
         secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-        region: process.env.S3_REGION,
-        endpoint: "https://s3.ap-northeast-1.amazonaws.com"
+        region: process.env.S3_REGION
     },
   },
   // Uncomment to add Stripe support.
